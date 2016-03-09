e989ad683dc609a68b2916b4bba7fb745aa774f0
zavidova <karina@Karinas-MacBook-Pro-9.local>
2016-03-09 17:30:31 +0100

    speech

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
@@ -6,33 +7,44 @@ var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
+//recognition.continuous = false;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector('#conclusion');
+var diagnostic = document.querySelector('.output');
 var bg = document.querySelector('html');
 
 document.body.onclick = function() {
- recognition.start();
- console.log('Ready to receive a color command.');}
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
 
 recognition.onresult = function(event) {
-  The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-   The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  It has a getter so it can be accessed like an array
-  the first [0] returns the SpeechRecognitionResult at position 0.
-  Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  These also have getters so they can be accessed like arrays.
-  The second [0] returns the SpeechRecognitionAlternative at position 0.
-  We then return the transcript property of the SpeechRecognitionAlternative object 
-var color = event.results[0][0].transcript;
- diagnostic.textContent = 'result: ' + color;
- bg.style.backgroundColor = color;//
- bg.style.color = color;
-console.log('Confidence: ' + event.results[0][0].confidence);}
-
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at position 0.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object 
+  var color = event.results[0][0].transcript;
+  diagnostic.textContent = 'result: ' + color;
+  //bg.style.backgroundColor = color;//
+  bg.style.color = color;
+  console.log('Confidence: ' + event.results[0][0].confidence);
+}
 
 recognition.onspeechend = function() {
-recognition.stop();
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = 'I didnt recognise that color.';
+}
+
+
+recognition.onerror = function(event) {
+ diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
